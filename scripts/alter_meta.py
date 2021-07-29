199c0b9c58c0f3fd3eb39afd02decb80ebc8374b
Brock Fanning <brockfanning@gmail.com>
2021-07-29 14:03:21 -0400

    Fix single quotes

--- scripts/alter_meta.py
+++ scripts/alter_meta.py
@@ -1,3 +1,10 @@
 def alter_meta(meta):
-    # Perform alterations of metadata or indicator config here.
+    if 'en' in meta:
+        for key in meta['en']:
+            if meta['en'][key] is not None and isinstance(meta['en'][key], str):
+                meta['en'][key] = meta['en'][key].replace("'", "&#39;")
+    for key in meta:
+        if meta[key] is not None and isinstance(meta[key], str):
+            meta[key] = meta[key].replace("'", "&#39;")
+
     return meta
